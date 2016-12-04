de6a0f9755b2a95ae195a2f706dc0a727199ed8e
Matt <kennedyl@uwec.edu>
2016-12-04 14:41:17 -0600

    Added queue level to mfqs process

--- mfqs_proc.h
+++ mfqs_proc.h
@@ -9,7 +9,7 @@ class MFQS_Proc : public Proc
 
 
   public:
-    MFQS_Proc(int in_pid = 0, int in_burst = 0, int in_arrival = 0, int in_priority = 0) //, int in_cost = 0 ) 
+    MFQS_Proc(int in_pid = 0, int in_burst = 0, int in_arrival = 0, int in_priority = 0, int queue = 0) //, int in_cost = 0 ) 
       : Proc(in_pid, in_burst, in_arrival, in_priority) //cost(in_cost)
     {
       pid = in_pid;
